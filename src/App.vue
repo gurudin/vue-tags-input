170e53c4ddec1dd441fd201630a42b9db9b30435
gaoxiang <hc.gaoxiang@gmail.com>
2018-09-30 15:36:15 +0800

    fixed add tags input

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,32 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
+    <h2>Examples</h2>
+    <hr>
+
+    <div>
+      <h4>bs3</h4>
+      <tags-input 
+       :bootstrap="3"
+       placeholder="Bootstrap 3"
+       :data-value="[1, 2, 3]"></tags-input>
+    </div>
+
+    <div>
+      <h4>bs4</h4>
+      <tags-input 
+       :bootstrap="4"
+       placeholder="Bootstrap 4"></tags-input>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+import TagsInput from "./TagsInput";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    TagsInput
   }
 }
 </script>
@@ -21,7 +36,6 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
 }
