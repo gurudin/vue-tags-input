fd00eee2270e16565a297068f8f6a583f374a37e
gaoxiang <hc.gaoxiang@gmail.com>
2018-10-09 18:31:51 +0800

    fixed build

--- src/TagsInput.vue
+++ src/TagsInput.vue
@@ -6,7 +6,7 @@
 
 <script>
 export default {
-  // name: 'InputTag',
+  name: 'tagsInput',
   props: {
     bootstrap: {
       type: Number,
--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ import tagsComponent from "./TagsInput.vue";
 
 const TagsInput = {
   install: function (Vue) {
-    if (typeof window !== 'undefined' && window.Vue) {
-      Vue = window.Vue
-    }
     Vue.component('TagsInput', tagsComponent)
   }
+}
+
+if (typeof window !== 'undefined' && window.Vue) {
+  window.Vue.use(TagsInput);
 }
 
 export default TagsInput
