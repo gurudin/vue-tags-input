7a5101d995b701cd315b77d4a5871e32ee11948d
gaoxiang <4008353@qq.com>
2018-12-17 18:12:19 +0800

    fixed tag input

--- src/App.vue
+++ src/App.vue
@@ -2,30 +2,12 @@
   <div id="app">
     <h2>Examples</h2>
     <hr>
+    <input type="text" class="form-control" placeholder="Username" ref="tagsinput">
+    <br>
 
-    <div>
-      <h4>bs3</h4>
-      <tags-input 
-       :bootstrap="3"
-       placeholder="Bootstrap 3"
-       :data-value="[1, 2, 3]"
-       label-style="warning"></tags-input>
-    </div>
-
-    <div>
-      <h4>bs4</h4>
-      <tags-input 
-       :bootstrap="4"
-       placeholder="Bootstrap 4"
-       :data-value="['A', 'B', 'C']"
-       label-style="dark"
-       :callback="bt4call"
-       ref="bs4"></tags-input>
-
-      <button type="button" class="btn btn-primary" @click="setTag">@setTag</button>
-      <button type="button" class="btn btn-primary" @click="getTag">@getTag</button>
-      <button type="button" class="btn btn-primary" @click="clearTag">@clearTag</button>
-    </div>
+    <tagsInput
+      v-model="options"
+      labelStyle="primary"></tagsInput>
   </div>
 </template>
 
@@ -35,31 +17,34 @@ import TagsInput from "./TagsInput";
 export default {
   name: 'App',
   data() {
-    return {};
+    return {
+      options: ['PHP', 'Javascript', 'Vue'],
+      // options: 'PHP,JAVA,GO',
+    };
   },
   methods: {
-    bt4call(tag, inx) {
-      console.log(tag, inx);
+    // bt4call(tag, inx) {
+    //   console.log(tag, inx);
       
-      var _this = this;
-      setTimeout(() => {
-        if (tag == 1) {
-          _this.$refs.bs4.removeTag(inx)
-        }
-      }, 1000);
+    //   var _this = this;
+    //   setTimeout(() => {
+    //     if (tag == 1) {
+    //       _this.$refs.bs4.removeTag(inx)
+    //     }
+    //   }, 1000);
 
-      return true;
-    },
-    setTag() {
-      var tags = ['D', 'E', 'F'];
-      this.$refs.bs4.setTag(tags);
-    },
-    getTag() {
-      console.log(this.$refs.bs4.getTag());
-    },
-    clearTag() {
-      this.$refs.bs4.clearTag();
-    }
+    //   return true;
+    // },
+    // setTag() {
+    //   var tags = ['D', 'E', 'F'];
+    //   this.$refs.bs4.setTag(tags);
+    // },
+    // getTag() {
+    //   console.log(this.$refs.bs4.getTag());
+    // },
+    // clearTag() {
+    //   this.$refs.bs4.clearTag();
+    // }
   },
   components: {
     TagsInput
@@ -68,35 +53,13 @@ export default {
 </script>
 
 <style>
+/* @import "https://cdn.bootcss.com/twitter-bootstrap/4.1.3/css/bootstrap.min.css"; */
+@import "https://cdn.bootcss.com/twitter-bootstrap/3.3.7/css/bootstrap.min.css";
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
-  margin-top: 60px;
-}
-.btn {
-  display: inline-block;
-  font-weight: 400;
-  text-align: center;
-  white-space: nowrap;
-  vertical-align: middle;
-  -webkit-user-select: none;
-  -moz-user-select: none;
-  -ms-user-select: none;
-  user-select: none;
-  border: 1px solid transparent;
-  padding: .375rem .75rem;
-  font-size: 1rem;
-  /* line-height: 1.5; */
-  cursor: pointer;
-  margin-top: 10px;
-  border-radius: .25rem;
-  transition: color .15s ease-in-out,background-color .15s ease-in-out,border-color .15s ease-in-out,box-shadow .15s ease-in-out;
-}
-.btn-primary {
-  color: #fff;
-  background-color: #007bff;
-  border-color: #007bff;
+  margin: 60px;
 }
 </style>
--- src/TagsInput.vue
+++ src/TagsInput.vue
@@ -1,6 +1,24 @@
 <template>
-  <div :class="bsStyle" @click="blur">
-    <span class="label" :class="'label-' + labelStyle" v-for="(item,inx) in tagsValue">{{item.tag}} <i data-role="remove" @click="removeTag(inx)"></i></span><input type="text" :size="inputSize" :placeholder="placeholder" v-model.trim="currentValue" @keyup="keyAction" ref="tagsinput" />
+  <div class="form-control" :style="boxStyle" @click="blur">
+
+    <span :class="'bg-'+labelStyle+' label-'+labelStyle" :style="badgeStyle" v-for="opt in dataValue">{{opt}} 
+      <button type="button" class="close" aria-label="Close" :style="removeStyle">
+        <i aria-hidden="true">&times;</i>
+      </button>
+    </span>
+
+    <input
+      type="text"
+      :style="inputStyle"
+      :placeholder="placeholder"
+      :size="inputSize"
+      v-model="currentValue"
+      @keyup="keyAction"
+      ref="tagsinput">
+
+    <input type="hidden" :value="value">
+
+    {{dataValue}}
   </div>
 </template>
 
@@ -8,40 +26,93 @@
 export default {
   name: 'tagsInput',
   props: {
-    bootstrap: {
-      type: Number,
-      default: 4,
-      required: false
+    /**
+     * 绑定数据
+     *
+     * @type {String|Array}
+     *
+     * @required false
+     */
+    value: {
+      type: null,
+      required: false,
+      default: ''
     },
+    /**
+     * 占位描述
+     * 
+     * @type {String}
+     * 
+     * @required false
+     */
     placeholder: {
       type: String,
-      required: false
+      required: false,
+      default: 'Enter tag'
     },
+    /**
+     * label 风格
+     * 
+     * @type {String}
+     * 
+     * @required false
+     */
     labelStyle: {
       type: String,
       default: 'info',
       required: false
     },
-    dataValue: {
-      type: null,
-      required: false
-    },
-    callback: {
-      type: null,
-      required: false
-    },
-    onChange: {
-      type: null,
-      required: false
-    },
   },
   data() {
     return {
-      tagsValue: Array,
-      currentValue: ''
+      currentValue: '',
     };
   },
   computed: {
+    dataValue: {
+      get() {
+        return typeof this.value == 'string'
+          ? this.value.split(",")
+          : this.value;
+      },
+      set() {
+
+      }
+    },
+    boxStyle() {
+      return {
+        'padding': '0 12px !important',
+      };
+    },
+    inputStyle() {
+      return {
+        'outline': 'none',
+        'border': '0!important',
+        'color': '#495057',
+        'height': '27px',
+        'margin-top': '3px',
+      };
+    },
+    badgeStyle() {
+      return {
+        'border-radius': '5px',
+        'padding': '5px 9px',
+        'margin': '-2px 3px 0 0',
+        'display': 'inline-block',
+        'font-size': '12px',
+        'font-weight': '700',
+        'line-height': '1',
+        'color': '#fff',
+        'vertical-align': 'middle',
+      };
+    },
+    removeStyle() {
+      return {
+        'font-size': '12px',
+        'margin-left': '10px',
+        'color': 'white'
+      };
+    },
     inputSize() {
       if (this.currentValue != '') {
         var reg = /[\u4e00-\u9fa5\uF900-\uFA2D]/; 
@@ -51,250 +122,16 @@ export default {
           return this.currentValue.length;
         }
       }
-
       return typeof this.placeholder == 'undefined' ? 1 : this.placeholder.length;
     },
-    bsStyle() {
-      return "input-tag-bs" + this.bootstrap;
-    }
   },
   methods: {
     blur() {
       this.$refs.tagsinput.focus();
     },
     keyAction(event) {
-      if (this.currentValue == '' && (event.key == 'ArrowLeft' || event.key == 'ArrowRight')) {
-        let current = event.target;
-        event.key == 'ArrowLeft'
-          ? (current.previousSibling != null ? current.previousSibling.before(current) : '')
-          : (current.nextSibling != null ? current.nextSibling.after(current) : '');
-
-        this.blur();
-
-        return false;
-      }
       
-      if (event.key == 'Enter' || event.key == ',' || event.key == 'Meta') {
-        let _this = this;
-        let tmpTagArr = [];
-        tmpTagArr = this.currentValue.split(',');
-
-        tmpTagArr.forEach(arr => {
-          let tmpArr = arr.split('，');
-          tmpArr.forEach(row => {
-            if (row != '') {
-              if (typeof _this.callback == 'function') {
-                let tagLen = _this.tagsValue.length;
-                _this.tagsValue.push({ tag: row, loading: true});
-
-                let res = _this.callback({action: 'add', value: row}, tagLen);
-                
-                res == true ? _this.tagsValue[tagLen].loading = false : _this.tagsValue.splice(tagLen, 1);
-              } else {
-                _this.tagsValue.push({ tag: row, loading: false});
-              }
-
-              if (typeof _this.onChange == 'function') {
-                _this.onChange(_this.getTag())
-              }
-            }
-          });
-        });
-
-        this.currentValue = '';
-      }
     },
-    removeTag(inx) {
-      if (typeof this.callback == 'function') {
-        if (this.callback({action: 'remove', value: this.tagsValue[inx].tag}, inx)) {
-          this.tagsValue.splice(inx, 1);
-        }
-      } else {
-        this.tagsValue.splice(inx, 1);
-      }
-
-      if (typeof this.onChange == 'function') {
-        this.onChange(this.getTag())
-      }
-    },
-    setTag(tags) {
-      let tagsValue = [];
-      tags.forEach((row, inx) => {
-        tagsValue[inx] = { tag: row, loading: false};
-      });
-
-      this.tagsValue = tagsValue;
-    },
-    getTag() {
-      let tagsValue = [];
-      this.tagsValue.forEach(row => {
-        tagsValue.push(row.tag);
-      });
-
-      return tagsValue;
-    },
-    clearTag() {
-      this.tagsValue = [];
-    }
-  },
-  created() {
-    if (typeof this.dataValue == 'undefined' || this.dataValue == '') {
-      this.tagsValue = [];
-    } else {
-      var tagsValue = [];
-      this.dataValue.forEach((row, inx) => {
-        tagsValue[inx] = { tag: row, loading: false };
-      });
-
-      this.tagsValue = tagsValue;
-    }
   }
 }
 </script>
-
-<style>
-.input-tag-bs3 [data-role="remove"], .input-tag-bs4 [data-role="remove"] {
-  margin-left: 5px;
-  cursor: pointer;
-}
-.input-tag-bs3 [data-role="remove"]:after, .input-tag-bs4 [data-role="remove"]:after {
-  content: "x";
-  padding: 0px 2px;
-}
-.input-tag-bs3 .label, .input-tag-bs4 .label {
-  display: block;
-  /* float: left; */
-  margin: 10px 0 0 10px;
-}
-
-.input-tag-bs3 {
-  display: block;
-  max-width: 100%;
-  min-height: 34px;
-  background-color: #fff;
-  background-image: none;
-  border: 1px solid #ccc;
-  border-radius: 4px;
-  -webkit-box-shadow: inset 0 1px 1px rgba(0,0,0,.075);
-  box-shadow: inset 0 1px 1px rgba(0,0,0,.075);
-  -webkit-transition: border-color ease-in-out .15s,-webkit-box-shadow ease-in-out .15s;
-  -o-transition: border-color ease-in-out .15s,box-shadow ease-in-out .15s;
-  transition: border-color ease-in-out .15s,box-shadow ease-in-out .15s;
-}
-.input-tag-bs3 input {
-  width: auto;
-  outline:none;
-  margin: 8px 10px;
-  border: none;
-  font: inherit;
-  font-family: inherit;
-  font-size: 14px;
-  line-height: 1.42857143;
-  color: #555;
-}
-.input-tag-bs3 input::placeholder {
-  color: #999;
-  opacity: 1;
-  font-weight: 800;
-}
-.input-tag-bs3 .label {
-  display: inline;
-  padding: .2em .6em .3em;
-  font-size: 75%;
-  font-weight: 700;
-  line-height: 1;
-  color: #fff;
-  text-align: center;
-  white-space: nowrap;
-  vertical-align: baseline;
-  border-radius: .25em;
-}
-.input-tag-bs3 .label-default {
-  background-color: #777;
-}
-.input-tag-bs3 .label-primary {
-  background-color: #337ab7;
-}
-.input-tag-bs3 .label-success {
-  background-color: #5cb85c;
-}
-.input-tag-bs3 .label-info {
-  background-color: #5bc0de;
-}
-.input-tag-bs3 .label-warning {
-  background-color: #f0ad4e;
-}
-.input-tag-bs3 .label-danger {
-  background-color: #d9534f;
-}
-
-.input-tag-bs4 {
-  display: block;
-  width: 100%;
-  min-height: calc(2.25rem + 2px);
-  background-color: #fff;
-  background-clip: padding-box;
-  border: 1px solid #ced4da;
-  border-radius: .25rem;
-  transition: border-color .15s ease-in-out,box-shadow .15s ease-in-out;
-}
-.input-tag-bs4 input {
-  width: auto;
-  outline:none;
-  border: none;
-  font: inherit;
-  font-family: inherit;
-  margin: 8px 10px;
-  font-size: 1rem;
-  color: #495057;
-}
-.input-tag-bs4 input::placeholder {
-  color: #6c757d;
-  opacity: 1;
-  font-weight: 700;
-}
-.input-tag-bs4 .label {
-  display: inline-block;
-  padding: .25em .4em;
-  font-size: 75%;
-  font-weight: 700;
-  line-height: 1;
-  text-align: center;
-  white-space: nowrap;
-  vertical-align: baseline;
-  border-radius: .25rem;
-}
-.input-tag-bs4 .label-primary {
-  color: #fff;
-  background-color: #007bff;
-}
-.input-tag-bs4 .label-secondary {
-  color: #fff;
-  background-color: #6c757d;
-}
-.input-tag-bs4 .label-success {
-  color: #fff;
-  background-color: #28a745;
-}
-.input-tag-bs4 .label-danger {
-  color: #fff;
-  background-color: #dc3545;
-}
-.input-tag-bs4 .label-warning {
-  color: #212529;
-  background-color: #ffc107;
-}
-.input-tag-bs4 .label-info {
-  color: #fff;
-  background-color: #17a2b8;
-}
-.input-tag-bs4 .label-light {
-  color: #212529;
-  background-color: #f8f9fa;
-}
-.input-tag-bs4 .label-dark {
-  color: #fff;
-  background-color: #343a40;
-}
-</style>
-
